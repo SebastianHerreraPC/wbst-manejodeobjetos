eed640776d8b4c9b41abf861b0c93b1579282d09
SebastianHerreraPc <sebehepe00@gmail.com>
2024-07-14 22:09:54 -0600

    objeto basico y notacion por puntos

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+//objeto basico
+const keretHouse = {
+  bedrooms: 3,
+  heat: "gas",
+  hasBasement: false,
+  architect: {
+    name: "Jakub Szczęsny",
+    homeCountry: "Poland",
+    yearOfBirth: 1973,
+  },
+};
+
+console.log(keretHouse);
+//notacion por puntos
+const obj = {
+  one: 1,
+  two: 2,
+  three: 3,
+};
+
+console.log(obj.three);
