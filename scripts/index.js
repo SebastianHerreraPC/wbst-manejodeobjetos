a1e6397b2c09d4573150b681e9c39f517cb86444
SebastianHerreraPc <sebehepe00@gmail.com>
2024-07-15 22:01:10 -0600

    opciones de lenguaje

--- scripts/index.js
+++ scripts/index.js
@@ -81,3 +81,27 @@ const welcomeMessages = {
 function addWelcomeMessage(language, message) {
   welcomeMessages[language] = message;
 }
+
+//ejercicio de numeros telefonicos
+
+const phonebook = {
+  Lily: {
+    mobile: "+16235554420",
+    work: "+12445552374",
+    home: "+19775552223",
+  },
+  "Kyle, el hermano de Tim": {
+    mobile: "+4235555616",
+  },
+};
+
+function addPhoneNumber(name, numberType, number) {
+  if (!phonebook[name]) {
+    phonebook[name] = {};
+  }
+  phonebook[name][numberType] = number;
+}
+// cuando la función esté lista, añade el número de teléfono de xx
+addPhoneNumber("Stephanie Noland", "móvil", "+4235555212");
+
+console.log(phonebook["Stephanie Noland"].móvil); // "+4235555212"
