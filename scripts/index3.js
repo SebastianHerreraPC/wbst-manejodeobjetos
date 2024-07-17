e0b680a800c874ea370f80ef858c78aa410d217c
SebastianHerreraPc <sebehepe00@gmail.com>
2024-07-16 22:32:38 -0600

    ejercicio2

--- scripts/index3.js
+++ scripts/index3.js
@@ -10,7 +10,60 @@ const mascotas = {
 for (animales in mascotas) {
   console.log(`${animales} y  ${mascotas[animales]}`);
 }
-
+//uso de object keys
 Object.keys(mascotas).forEach(function (a) {
   console.log(a);
 });
+
+//ejercicio:
+const heights = {
+  burjKhalifa: 828,
+  tokyoSkyTree: 634,
+  shanghaiTower: 632,
+  abrajAlBait: 601,
+  cantonTower: 600,
+  pingAnFinanceCentre: 600,
+  lotteWorldTower: 555,
+  cnTower: 553,
+  oneWorldTradeCenter: 541,
+  ostankinoTower: 540,
+};
+
+const stratosphere = 11000;
+
+const sumHeights = Object.values(heights).reduce(function (current, result) {
+  return current + result;
+}, 0);
+
+console.log(sumHeights >= stratosphere);
+
+//ejercicio 2
+
+const tion = {
+  fruition: "la realización o cumplimiento de un plan o proyecto",
+  depreciation: "el proceso de pérdida de valor",
+  microseismicity: "un débil temblor de tierra causado por fenómenos naturales",
+  alienation: "la transmisión de la propiedad",
+  bifurcation:
+    "el punto o la zona en la que algo se divide en dos ramas o partes",
+  locomotion: "la capacidad de moverse",
+  fusion: "proceso de unificar o juntar dos objetos",
+};
+
+Object.keys(tion).forEach(function (key) {
+  if (!key.endsWith("tion")) delete tion[key];
+});
+
+console.log(tion);
+
+/*
+  
+    {
+      fruition: "la realización o cumplimiento de un plan o proyecto",
+      depreciation: "el proceso de pérdida de valor",
+      alienation: "la transmisión de la propiedad",
+     bifurcation: "el punto o la zona en la que algo se divide en dos ramas o partes",
+      locomotion: "la capacidad de moverse"
+    }
+  
+  */
