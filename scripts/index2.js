4731e363ff5a1d4c253f64b89d4cd31ff18d96e6
SebastianHerreraPc <sebehepe00@gmail.com>
2024-07-16 21:48:17 -0600

    nuevo directorio y ejercicio

--- scripts/index2.js
+++ scripts/index2.js
@@ -14,3 +14,38 @@ const animales = {
   mascotaDos: "gato",
 };
 console.log("mascotaUno" in animales);
+
+//ejercicio de eliminacion
+const right = {
+  right1: "⇸",
+  right2: "→",
+  notLeft: "⇍",
+  right3: "⇛",
+  right4: "⇉",
+  left: "⇐",
+};
+
+delete right.left;
+
+console.log(right);
+
+//ejercicio
+const welcomeMessages = {
+  english: "Welcome",
+  french: "Bienvenue",
+  italian: "Benvenuto",
+  spanish: "Bienvenido",
+  russian: "Добро пожаловать",
+  chinese: "歡迎",
+  finnish: "Tervetuloa",
+};
+
+function deleteWelcomeMessages(propsArr) {
+  propsArr.forEach(function (key) {
+    delete welcomeMessages[key];
+  });
+}
+
+deleteWelcomeMessages(["italian", "french"]);
+
+console.log(welcomeMessages);
