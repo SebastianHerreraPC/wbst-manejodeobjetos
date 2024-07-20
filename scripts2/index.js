cfc4a95b9543f216db4d819e178c9fb75018ca58
SebastianHerreraPc <sebehepe00@gmail.com>
2024-07-19 22:28:12 -0600

    keypress y keyup

--- scripts2/index.js
+++ scripts2/index.js
@@ -4,5 +4,15 @@
 const inputTeclado = document.querySelector(".form__input-teclado");
 
 inputTeclado.addEventListener("keydown", function () {
-  console.log("Cada vez que pulses una tecla, me registro");
+  console.log("Estas pulsando una tecla cualquiera");
 });
+inputTeclado.addEventListener("keypress", function () {
+  console.log(
+    "Estas presionando otra, recuerda que los modificadores no cuentan"
+  );
+});
+inputTeclado.addEventListener("keyup", function () {
+  console.log("Dejaste de presionar la tecla");
+});
+
+//detectores y controladores
